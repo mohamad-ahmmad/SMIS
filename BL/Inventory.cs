77c8bfdfbf39f9a2264984be9be8429ee29076a2
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-07-23 20:51:26 +0300

    ✅Add "AddProduct" Method to Inventory static class
    💥Access the products in the inventory and add products by using AddProduct.

---
+++ BL/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public static class Inventory
+    {
+        static Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        /// <summary>
+        /// Add product to the inventory
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns>false or true, false if the product exists in the inventory</returns>
+        public static bool AddProduct(Product product)
+        {
+            Product res = Search(product);
+            if (res != null)
+                return false;
+
+            products.Add(product.Name, product);
+            return true;
+        }
+        public static List<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();   
+        }
+        public static Product Search (Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
