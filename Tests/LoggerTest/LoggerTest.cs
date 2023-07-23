59fcd86a65b6be657ccb476ea10855404c0c3e46
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-07-23 20:12:34 +0300

    ✅ Add Tests for Logging formating for products.

---
+++ Tests/LoggerTest/LoggerTest.cs
@@ -0,0 +1,61 @@
+﻿using BL;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace LoggerTest
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void LogProductTest()
+        {
+            var product = new Product()
+            {
+                Name = "Chips",
+                Price = 14.56M,
+                Quantity = 3
+            };
+
+            var actual = product.Log();
+
+            var expected = "Name : Chips, Price : 14.56, Quantity : 3";
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void LogListOfProductTest() 
+        {
+            var list = new List<Product>()
+            {
+            new Product()
+            {
+                Name = "Chips",
+                Price = 14.56M,
+                Quantity = 3
+            },
+            new Product()
+            {
+                Name = "Botato",
+                Price = 13.56M,
+                Quantity = 3
+            }
+        };
+
+            var act = "";
+
+            foreach(var item in list)
+            {
+                act +=$"{item.Log()}\n";
+            }
+
+            var expected = "Name : Chips, Price : 14.56, Quantity : 3\n" +
+                            "Name : Botato, Price : 13.56, Quantity : 3\n";
+
+            Assert.AreEqual(expected, act);
+        }
+    }
+}
---
+++ Tests/LoggerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LoggerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ce5829f-0ce0-4306-9075-df98883c4ea2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
