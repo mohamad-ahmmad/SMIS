bc51d6b5eb6422621530c4bb2cd3100afd4c7967
Mohammad Ahmad <eng.2002.mohammad@gmail.com>
2023-07-23 19:37:33 +0300

    Add Logger and ILoggable 💥
    ✅Start a new project "Common" in SIMS solution.
    ✅Logger and ILoggable Added to deal with logging.
    ✅Implements ILoggable in Product class.

---
+++ BL/Product.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Product : ILoggable
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public string Log()
+        {
+            return $"Name : {Name}, Price : {Price}, Quantity : {Quantity}";
+        }
+
+
+    }
+}
---
+++ Common/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Logger
+    {
+        public static void LogToConsole(List<ILoggable> list)
+        {
+            foreach (ILoggable loggable in list)
+                Logger.LogToConsole(loggable);
+        }
+
+        public static void LogToConsole(ILoggable loggable)
+        {
+                Console.WriteLine(loggable.Log());
+        }
+    }
+}
